8f7e2ba643aaf94ae717a6f4416aabd778975109
suraj <suraj@funnelstory.io>
2023-10-04 16:00:38 +0530

    new elysia app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
+// http.ts
+const vmRegion = process.env.FLY_REGION || "local";
+console.log(`Doing it from ${vmRegion}`);
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", (c) => "ok")
+  .use(
+    jwt({
+      name: "jwt",
+      secret: "Fischl von Luftschloss Narfidort",
+    })
+  )
+  .use(cookie())
+  .get("/sign/:name", async ({ jwt, cookie, setCookie, params }) => {
+    setCookie("auth", await jwt.sign(params), {
+      httpOnly: true,
+      maxAge: 7 * 86400,
+    });
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    return `Sign in as ${cookie.auth}`;
+  })
+  .get("/profile", async ({ jwt, set, cookie: { auth } }) => {
+    const profile = await jwt.verify(auth);
+
+    if (!profile) {
+      set.status = 401;
+      return "Unauthorized";
+    }
+
+    return `Hello ${profile.name}`;
+  })
+  .get("/logout", ({ removeCookie }) => {
+    removeCookie("auth");
+    return `ok`;
+  })
+  .listen({
+    hostname: "::",
+    port: process.env.PORT ?? 3000,
+  });
+
+export type App = typeof app;
