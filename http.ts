e0f243fd363884f6249e3c68dc3a4bd4fe81bafb
suraj <suraj@funnelstory.io>
2023-10-04 09:56:20 +0530

    add jwt token

--- http.ts
+++ http.ts
@@ -1,6 +1,7 @@
 import { Elysia, t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
 // http.ts
 const vmRegion = process.env.FLY_REGION || "local";
 console.log(`Doing it from ${vmRegion}`);
@@ -8,8 +9,22 @@ console.log(`Doing it from ${vmRegion}`);
 const app = new Elysia()
   .use(swagger())
   .get("/", (c) => "ok")
-
+  .use(
+    jwt({
+      name: "jwt",
+      secret: "Fischl von Luftschloss Narfidort",
+    })
+  )
   .use(cookie())
+  // @ts-ignore
+  .get("/sign/:name", async ({ jwt, cookie, setCookie, params }) => {
+    setCookie("auth", await jwt.sign(params), {
+      httpOnly: true,
+      maxAge: 7 * 86400,
+    });
+
+    return `Sign in as ${cookie.auth}`;
+  })
   // @ts-ignore
   .get("/profile", async ({ jwt, set, cookie: { auth } }) => {
     const profile = await jwt.verify(auth);
@@ -20,6 +35,11 @@ const app = new Elysia()
     }
 
     return `Hello ${profile.name}`;
+  })
+  //@ts-ignore
+  .get("/logout", ({ removeCookie }) => {
+    removeCookie("auth");
+    return `ok`;
   })
   .listen({
     hostname: "::",
